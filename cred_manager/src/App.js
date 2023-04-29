4b17fe1e1c9d65b43bc8ccfbe67bbb28f70674c0
Rushikesh Pitale <pitalerushikesh@gmail.com>
2023-04-29 11:28:05 +0530

    initial commit

---
+++ cred_manager/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Grid } from "@material-ui/core";
+
+function App() {
+  return (
+    <Grid container>
+      <Grid xs={12}>
+        <h1>Login</h1>
+      </Grid>
+      <Grid xs={12}>
+        <h1>Password</h1>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
